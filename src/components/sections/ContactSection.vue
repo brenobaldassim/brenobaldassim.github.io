d612bba67eb7145312ca2a156e00d4c521950bb7
Breno Baldassim <brenobaldassim.contato@gmail.com>
2024-06-02 21:39:42 -0300

    setting contact first structure

--- src/components/sections/ContactSection.vue
+++ src/components/sections/ContactSection.vue
@@ -4,19 +4,23 @@ import SectionLayout from '../utils/SectionLayout.vue'
 
 <template>
   <SectionLayout>
-    <div class="text-xl md:text-2xl text-left w-full mt-4 md:mt-20">
-      <form>
+    <div class="flex flex-col items-center justify-center w-full text-xl md:text-2xl mt-4 md:mt-8">
+      <form class="md:w-1/3 bg-app-base rounded border border-app-secondary p-4 md:p-8">
         <div class="mb-3">
-          <label></label>
+          <label class="mb-2">email:</label>
           <input type="text" placeholder="email" />
         </div>
-        <div class="mb-3">
-          <label></label>
-          <input type="text" placeholder="message" />
+        <div class="md:mb-12 mb-8">
+          <label class="mb-2">message:</label>
+          <textarea type="text" placeholder="message" />
         </div>
       </form>
     </div>
   </SectionLayout>
 </template>
 
-<style scoped></style>
+<style scoped>
+input, textarea{
+  border-radius: 4px;
+}
+</style>
