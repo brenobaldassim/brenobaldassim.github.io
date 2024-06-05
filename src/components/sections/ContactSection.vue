08f109515400dc2cd398f6ab04e6513fdfc07eab
Breno Baldassim <brenobaldassim.contato@gmail.com>
2024-06-05 08:30:21 -0300

    format

--- src/components/sections/ContactSection.vue
+++ src/components/sections/ContactSection.vue
@@ -2,15 +2,15 @@
 import SectionLayout from '../utils/SectionLayout.vue'
 
 const submitEmail = (e) => {
-  console.log(e.target);
+  console.log(e.target)
 }
 </script>
 
 <template>
   <SectionLayout>
-    <div class="flex flex-col items-center justify-center w-full text-xl md:text-2xl md:mt-4 font-bold">
-      <form ref="emailForm" @submit.prevent="submitEmail" class="md:w-2/4 p-4 md:p-8">
-        <div class="text-center mb-12 bg-app-base p-8 rounded">
+    <div class="flex flex-col items-center justify-center w-full text-xl md:mt-4 font-bold">
+      <form ref="emailForm" @submit.prevent="submitEmail" class="md:w-2/4 p-4">
+        <div class="text-center mb-12 bg-app-base p-6 rounded">
           <h1 class="text-4xl">Have you liked my portifolio?</h1>
           <h2 class="text-2xl">Send me an email!</h2>
         </div>
@@ -24,14 +24,20 @@ const submitEmail = (e) => {
             <textarea type="text" placeholder="Hi, I would like to..." />
           </div>
         </div>
-        <button type="submit" class=" text-lg w-full bg-app-base rounded hover:bg-app-base-light p-2">SEND EMAIL</button>
+        <button
+          type="submit"
+          class="text-lg w-full bg-app-base rounded hover:bg-app-base-light p-2"
+        >
+          SEND EMAIL
+        </button>
       </form>
     </div>
   </SectionLayout>
 </template>
 
 <style scoped>
-input, textarea{
+input,
+textarea {
   font-size: 16px;
   border-radius: 4px;
   background-color: var(--app-color-base);
